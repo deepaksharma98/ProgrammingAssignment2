ac4201cad71b00e95df21b1f4a7830a6dde67926
Deepak kumar sharma <52253039+deepaksharma98@users.noreply.github.com>
2020-06-21 13:54:58 +0530

    catch.R

---
+++ catch.R
@@ -0,0 +1,52 @@
+## The following pair of functions can cache the inverse of a matrix
+
+## This function creates a special "matrix" object that can cache 
+## its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
+}
+
+
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
+
+
+#TestingMatrix
+
+colyn <- matrix(c(1,2,3,5), 2,2)
+
+papa <- makeCacheMatrix(colyn)
+
+#testing
+cacheSolve(papa)
+
+#cache?
+
+cacheSolve(papa)
+
